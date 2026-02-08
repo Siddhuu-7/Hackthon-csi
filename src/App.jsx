cdca372b660ec6ed61704194a8f2b7c459f9d178
Siddhardha Utla <siddhardhautla19@gmail.com>
2026-02-08 15:25:00 +0530

    idea submision page is mofied

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 
 import Homepage from './pages/Home';
 import Regestration from "./pages/RegestrationPage"
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import ProblemStatement from './pages/ProblemStatement';
+import IdeaSubmission from './pages/IdeaSubmission';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Paymentpage from './pages/paymentpage';
 import AdminPage from './pages/AdminPage';
+import Preloader from './components/Preloader';
+import { AnimatePresence } from 'framer-motion';
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handlePreloaderComplete = useCallback(() => {
+    setIsLoading(false);
+  }, []);
+
   return (
     <BrowserRouter >
+    <AnimatePresence mode="wait">
+      {isLoading && <Preloader onComplete={handlePreloaderComplete} />}
+    </AnimatePresence>
     <Routes >
       <Route element={<Homepage/>} path='/'/>
       <Route element={<Regestration/>} path='/registration'/>
       <Route element={<ProblemStatement/>} path='/statement'/>
+      <Route element={<IdeaSubmission/>} path='/idea-submission'/>
       <Route element={<Paymentpage/>}  path='/payment'/>
-<Route element={<AdminPage />} path="/admin-7842xk29" />
+      <Route element={<AdminPage />} path="/admin-7842xk29" />
     </Routes>
     </BrowserRouter>
   )
